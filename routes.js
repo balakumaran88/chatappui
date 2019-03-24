8b413856877fa8d6ee1dfbb31d6a63d675d74fd7
balakumaran88 <bala.mg88@gmail.com>
2019-03-24 12:25:53 +0530

    final commit

--- routes.js
+++ routes.js
@@ -1,9 +1,157 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router();
+const User = require('./model/user')
+const jwt = require('jsonwebtoken')
+
+
+function verifyToken(req, res, next){
+  if(!req.headers.authorization) {
+      return res.status(401).send("Unauthorized Request")
+  } else {
+      let token = req.headers.authorization.split(' ')[1]
+      if(token == null){
+          return res.status(401).send("Unauthorized Request")
+      } else {
+         if(token !== null) {
+          let payload = jwt.verify(token, 'secretkey')
+          req.userId = payload ?  payload.subject : ''
+          if(!req.userId) {
+              return res.status(401).send("Unauthorized Request")
+          }
+         } else {
+          return res.status(401).send("Unauthorized Request")
+         }
+          
+          next() 
+      }
+    next()
+  }
+  
+}
+
 
 router.get('/', (req, res) => {
     res.send('Hi All')
 })
 
+router.post('/register', (req,res) => {
+    const userData = req.body;
+    let user = new User(userData)
+    user.save((err,docs ) => {
+        if(err) {
+            console.log(err)
+        } else {
+          let payload = {subject: res._id}
+          let token = jwt.sign(payload, "secretkey")
+            res.status(200).send({token})
+        }
+    })
+})
 
+
+router.post('/login', (req,res) => {
+    let userData = req.body
+    User.findOne({email:userData.email},(err,resp) => {
+        if(resp){
+            if(resp.password == userData.password) {
+                let payload = { subject: resp._id}
+                let token = jwt.sign(payload,"secretkey")
+                res.status(200).send({token})
+            } else {
+                console.log('user not found')
+                res.status(401).send("Incorrect password")
+            }
+        } else {
+            console.log('user not found')
+            res.status(401).send( "user not found")
+        }
+    })
+})
+
+router.get('/events', (req,res) => {
+    let events = [
+        {
+          "_id": "1",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+          "_id": "2",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+          "_id": "3",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+          "_id": "4",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+          "_id": "5",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+          "_id": "6",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        }
+      ]
+
+      res.status(200).json(events)
+})
+
+
+router.get('/special', verifyToken, (req,res) => {
+    let events = [
+        {
+          "_id": "1",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+          "_id": "2",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+          "_id": "3",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+          "_id": "4",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+          "_id": "5",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+          "_id": "6",
+          "name": "Auto Expo",
+          "description": "lorem ipsum",
+          "date": "2012-04-23T18:25:43.511Z"
+        }
+      ]
+
+      res.status(200).json(events)
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 const express = require('express')
-var cors = require('cors')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express();
-var server = require('http').Server(app);
-const socket = require('socket.io')
-const io = socket(server)
+var server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const {db} = require('./config/db')
 
 
 
+io.on('connection', (socket) => {
+    console.log('one user connected', socket.id)
+    socket.on('join',(data) => {
+        socket.join(data.room)
+        console.log(`${data.user} has joined the ${data.room}` )
+        socket.broadcast.to(data.room).emit('user', {user: data.user, message: "has joined the room"})
+    })
+
+    socket.on('leave',(data) => {
+        console.log(`${data.user} has left the ${data.room}` )
+        socket.broadcast.to(data.room).emit('left', {user: data.user, message: "has left the room"})
+        socket.leave(data.room)
+    })
+
+})
+
+
+
+app.use(bodyParser.json())
 const routes = require('./routes')
   
-app.use(cors())
+app.use(cors({credentials:false, origin: 'http://localhost:4200'}))
 //using cross browser compatability
 
 //configuring routes
@@ -20,6 +40,6 @@ app.use('/api', routes)
 
 
 
-app.listen(3001, (err, resp) => {
+server.listen(3001, (err, resp) => {
     console.log('server is running on port 3001')
 })
